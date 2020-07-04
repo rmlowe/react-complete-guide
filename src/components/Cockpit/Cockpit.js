78e7e40d05716b86d81282683545a985792bddd6
Robert Lowe <robertmlowe@rmlowe.com>
2020-07-04 10:17:22 +0800

    Controlling the useEffect() Behavior

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -6,7 +6,12 @@ const cockpit = (props) => {
     useEffect(() => {
         console.log('[Cockpit.js] useEffect');
         // Http request...
-    });
+        setTimeout(() => {
+            alert('Saved data to cloud!');
+        }, 1000);
+    }, []);
+
+    // useEffect();
 
     const assignedClasses = [];
     let btnClass = '';
